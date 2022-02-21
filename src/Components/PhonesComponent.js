f3b84e34fb3b2d56c1a939b12f6311eec0d9a7a8
Jean Marie <jmrandrianant@gmail.com>
2022-02-21 22:11:58 +0300

    The useSelector is the simplified version of mapStateToProps, it simplifies our code

--- src/Components/PhonesComponent.js
+++ src/Components/PhonesComponent.js
@@ -1,34 +1,73 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import phone from '../images/phone.jpg';
 import {buyPhone} from '../Redux/Phone/actionPhone';
 
 
-const PhonesComponent = ({buyPhone, phones}) => {
+const PhonesComponent = () => {
+    // the equivalent hook of useStateToProps is useSelector
+    const phones = useSelector((state) => state.phones
+    )
     return (
         <div className='container'>
             <img src={phone} alt="phone" />
             <p>
                 Available :
-                <span id='count'> {phones}</span>
+                <span id='count'>{phones} </span>
             </p>
-            <button onClick={() => buyPhone()}>Acheter</button>
+            <button>Acheter</button>
         </div>
     );
 };
 
-const mapStateToProps = (state) => {
-  return {
-       phones:state.phones
-  }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//        phones:state.phones
+//   }
+// }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    buyPhone: () => {
-      dispatch(buyPhone())
-    }
-  }
-}
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     buyPhone: () => {
+//       dispatch(buyPhone())
+//     }
+//   } 
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PhonesComponent);
+export default PhonesComponent;
+
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import phone from '../images/phone.jpg';
+// import {buyPhone} from '../Redux/Phone/actionPhone';
+
+
+// const PhonesComponent = ({buyPhone, phones}) => {
+//     return (
+//         <div className='container'>
+//             <img src={phone} alt="phone" />
+//             <p>
+//                 Available :
+//                 <span id='count'> {phones}</span>
+//             </p>
+//             <button onClick={() => buyPhone()}>Acheter</button>
+//         </div>
+//     );
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//        phones:state.phones
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     buyPhone: () => {
+//       dispatch(buyPhone())
+//     }
+//   } 
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(PhonesComponent);
