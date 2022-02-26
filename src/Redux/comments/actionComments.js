b080cad3a41dd1ef735f980cd08d0791007571be
Jean Marie <jmrandrianant@gmail.com>
2022-02-26 09:50:14 +0300

    using the hooks useSelector() and useDispatch instead of mapStateToProps and mapDispatchToProps. Using of async/await instead of then() in the actionComments

--- src/Redux/comments/actionComments.js
+++ src/Redux/comments/actionComments.js
@@ -21,24 +21,35 @@ export const loadApiCommentSuccess = (comments) => {
     }
   }
 
+// export const apiCall = () => {
+//   return (dispatch) => {
+
+//     // let's dispatch the 1st action:
+//     dispatch(loadApiComment())
+//     axios.get('https://jsonplaceholder.typicode.com/comments').then((res) => {
+//       // dispatch loadApiCommentSuccess when the request is success
+
+//       // dispatch the 2nd action:
+//       dispatch(loadApiCommentSuccess(res.data))
+//     })
+//     .catch((err) => {
+
+//       // dispatch the 3rd action
+//         dispatch(loadApiCommentError(err.message))
+//     })
+//   }
+// }
+
 export const apiCall = () => {
-  return (dispatch) => {
-
-    // let's dispatch the 1st action:
-    dispatch(loadApiComment())
-    axios.get('https://jsonplaceholder.typicode.com/comments').then((res) => {
-      // dispatch loadApiCommentSuccess when the request is success
-
-      // dispatch the 2nd action:
-      dispatch(loadApiCommentSuccess(res.data))
-    })
-    .catch((err) => {
-
-      // dispatch the 3rd action
-        dispatch(loadApiCommentError(err.message))
-    })
+  return async (dispatch) =>{
+    try{
+      dispatch(loadApiComment())
+      const res = await axios.get('https://jsonplaceholder.typicode.com/comments');
+      const data = await dispatch(loadApiCommentSuccess(res.data))
+      return data;
+    }
+  catch(err) {
+    dispatch(loadApiCommentError(err.message))
+    }
   }
-}
-
- 
- 
+} 
